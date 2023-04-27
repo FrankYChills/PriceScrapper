4d8d903e3718c025b7e9a0705790fbc3316f1238
franky <itskchills11@gmail.com>
2023-04-27 10:31:47 +0530

    send html file on home page

--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const path = require("path");
+
 const main = require("../scrapeData");
+
+// default page
+router.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
 
 // get the request
 router.get("/price", async (req, res) => {
